7f69cbeb4cb99d376de3f8030271449644a177c1
Димка Стиклов <dimkastiklov@Dimkas-MacBook-Pro.local>
2023-09-15 15:04:43 +0400

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num =  Math.round((this.max - this.min) / 2 + this.min);
+        return this.num;
     }
 
     lower() {
-
+        this.max = this.num;
     }
 
     greater() {
-
+        this.min = this.num;
     }
 }
 
